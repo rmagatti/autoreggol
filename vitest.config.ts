ce4435282cb71a3e92a53563f238f4b01b9dee5e
Ronnie Magatti <ronniemagatti@gmail.com>
2022-09-27 12:50:19 -0600

    Switch to npm

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    include: ["test/**/*.test.ts"],
+    environment: "node",
+    threads: false,
+    logHeapUsage: true,
+    globalSetup: "./test/setup.ts",
+    coverage: {
+      reporter: ["lcov", "text", "html"],
+      reportsDirectory: "./coverage",
+    },
+  },
+});
